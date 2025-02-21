f72208d910ed3f43e21f96a3e7e95fca7607c9b2
Afred26 <schall.fredrik@gmail.com>
2025-02-21 14:29:15 +0100

    search implementiert

--- search/binfind.go
+++ search/binfind.go
@@ -4,6 +4,30 @@ package search
 // Falls x nicht gefunden wird, wird -1 zurückgegeben.
 // Da die Liste sortiert ist, wird die binäre Suche verwendet.
 func FindSorted(list []int, x int) int {
-	// TODO
-	return -1
+	m := len(list) / 2
+	if len(list) <= 1 {
+		if list[0] == x {
+			return 0
+		} else {
+			return -1
+		}
+	}
+	if list[m] == x {
+		return m
+	} else if list[m] < x {
+		n := FindSorted(list[m:], x)
+		if n >= 0 {
+			return n + m
+		} else {
+			return -1
+		}
+	} else {
+		n := FindSorted(list[:m], x)
+		if n >= 0 {
+			return n
+		} else {
+			return -1
+		}
+	}
+
 }
--- search/find.go
+++ search/find.go
@@ -4,6 +4,14 @@ package search
 // und gibt dessen Index zurück. Falls x nicht gefunden wird,
 // wird -1 zurückgegeben.
 func Find(list []int, x int) int {
-	// TODO
-	return -1
+	if len(list) == 0 {
+		return -1
+	}
+	if list[0] == x {
+		return 0
+	}
+	if Find(list[1:], x) == -1 {
+		return -1
+	}
+	return Find(list[1:], x) + 1
 }
